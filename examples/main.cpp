31d3c962abf0d47a15e61f8e69e81f07e489fe47
3D Maker Garage <3dmakergarage@gmail.com>
2024-12-08 14:32:52 +0100

    Update Example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,3 +1,28 @@
+/*
+ * Copyright 3D Maker Garage 2024
+ *
+ * This file is part of some open source application.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * Contact: 3dmakergarage@gmail.com
+ */
+
 #include <Arduino.h>
 #include <Lion.h>
 
@@ -5,9 +30,9 @@ unsigned int counter = 0;
 Lion lion = Lion();
 
 void displayCounter() {
-    lion.display()->clear();
-    lion.display()->writeAlignedText(20, 3, CENTER, (String)counter);
-    lion.display()->draw();
+    lion.display_clear();
+    lion.display_writeAlignedText(3, (String)counter);
+    lion.display_draw();
 }
 
 void incrementCounter() {
@@ -41,9 +66,11 @@ void onTouchButtonEvent(TouchButtons::BUTTON button, TouchButtons::BUTTON_EVENT 
 
 void setup() {
     lion.begin();
+    lion.setOnTouchButtonEvent(onTouchButtonEvent);
+    
     displayCounter();
 }
 
 void loop() {
-    lion.touchButtons()->loop(onTouchButtonEvent);
+    lion.loop();
 }
